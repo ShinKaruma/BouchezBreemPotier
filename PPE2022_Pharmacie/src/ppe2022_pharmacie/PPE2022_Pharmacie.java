b1f434d6aeeab66ce553d3c227454c5d4bdcfda3
ShinKaruma <77399452+ShinKaruma@users.noreply.github.com>
2021-09-17 12:04:22 +0200

    ajout de la fonction de connexion buggée
    fck le dev

--- PPE2022_Pharmacie/src/ppe2022_pharmacie/PPE2022_Pharmacie.java
+++ PPE2022_Pharmacie/src/ppe2022_pharmacie/PPE2022_Pharmacie.java
@@ -3,7 +3,16 @@ package ppe2022_pharmacie;
 public class PPE2022_Pharmacie {
 
     public static void main(String[] args) {
-        PasserelleAuth.Connection();
+        PasserelleAuth.ConnectionBDD();
+        
+        /**
+        *droits:
+        * 1 = pharmacien
+        * 2 = infirmier
+        * 3 = admin 
+        * */
+        
+        PasserelleAuth.Auth("Valentin", "12345");
     }
     
 }
--- PPE2022_Pharmacie/src/ppe2022_pharmacie/PasserelleAuth.java
+++ PPE2022_Pharmacie/src/ppe2022_pharmacie/PasserelleAuth.java
@@ -2,12 +2,20 @@ package ppe2022_pharmacie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PasserelleAuth {
     private static String url = "jdbc:postgresql://192.168.1.245:5432/PPE2022_Hopital_Pharmacie_BBP";
     private static String user = "potier";
     private static String passwd = "potier";
     private static Connection pdo;
+
+    private static Object outputgetClass() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     public PasserelleAuth(String pUrl, String pUser, String pPasswd){
@@ -16,7 +24,7 @@ public class PasserelleAuth {
         passwd = pPasswd;
     }
     
-    public static void Connection() {
+    public static void ConnectionBDD() {
         //Etablir connexion
         try {
             pdo = DriverManager.getConnection(url, user, passwd);
@@ -26,5 +34,17 @@ public class PasserelleAuth {
             System.out.println("Connexion refusé !");
             System.out.println(e.getMessage());
         }
+    }
+    
+    public static void Auth(String login, String password){
+        try {
+        Statement state = pdo.createStatement();
+        String requete = "SELECT count(*) FROM authentification WHERE login = "+login+" AND password = "+password;
+        ResultSet output = state.executeQuery(requete);
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Erreur login");
+        }
+        
     }
 }
